3c93ca334ab90a8cdfc6ca15caf309fb12f8733b
Andrews Queiroz <andrewss.queiroz@gmail.com>
2026-09-16 19:45:32 -0300

    Commit Inicial

---
+++ rota-vital/src/test/java/io/github/_ndrewss/rota_vital/RotaVitalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github._ndrewss.rota_vital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotaVitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
