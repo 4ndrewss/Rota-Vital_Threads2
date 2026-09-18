b47d50724983059f3eec7dee0473597cecc4e7b2
Andrews Queiroz <andrewss.queiroz@gmail.com>
2026-09-18 15:05:36 -0300

    add: Lógica de dividir a lista em N pedaços

--- src/main/java/io/github/_ndrewss/rota_vital/service/EstatisticaService.java
+++ src/main/java/io/github/_ndrewss/rota_vital/service/EstatisticaService.java
@@ -1,5 +1,6 @@
 package io.github._ndrewss.rota_vital.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,5 +29,41 @@ public class EstatisticaService {
         }
 
         return mapaEstatisticas;
+    }
+
+    public List<List<Requisicao>> dividirLista(List<Requisicao> requisicoes, int numThreads) {
+
+        // Lista que vai guardar os pedaços (cada pedaço é uma lista menor)
+        List<List<Requisicao>> pedacos = new ArrayList<>();
+
+        int tamanhoTotal = requisicoes.size();
+
+        // Quantos itens cada pedaço vai ter, em média
+        // Ex.: 100 itens / 4 threads = 25 itens por pedaço
+        int tamanhoPedaco = tamanhoTotal / numThreads;
+
+        int inicio = 0;
+
+        for (int i = 0; i < numThreads; i++) {
+
+            int fim = inicio + tamanhoPedaco;
+
+            // Na ÚLTIMA thread, garante que pega todo o resto,
+            // caso a divisão não seja exata (ex.: 101 itens / 4 threads)
+            if (i == numThreads - 1) {
+                fim = tamanhoTotal;
+            }
+
+            // subList() pega um pedaço da lista original, do índice "inicio" (incluso)
+            // até o índice "fim" (excluído)
+            List<Requisicao> pedaco = requisicoes.subList(inicio, fim);
+
+            pedacos.add(pedaco);
+
+            // O próximo pedaço começa onde esse terminou
+            inicio = fim;
+        }
+
+        return pedacos;
     }
 }
